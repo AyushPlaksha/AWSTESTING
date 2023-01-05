0e4346c580d3252abb381bb432bd3ce5ee180307
AyushPlaksha <ayush.binjola@plaksha.edu.in>
2023-01-05 12:19:38 +0530

    first commit

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import '../../public/style.css'
+import logo from '../../Images/logo-green.png'
+import Form from '../../Pages/Form'
+
+class Navbar extends Component {
+
+ 
+
+  constructor(props) {
+    super(props)
+    console.log(this.props)
+    this.state = {
+      message: "This is data",
+      video: true,
+    }
+  }
+
+  chnageBg = () => {
+    if (this.state.video) {
+      this.setState({
+        video: false,
+      })
+    } else {
+      this.setState({
+        video: true,
+      })
+    }
+  }
+
+    render(){
+      const LISTS =['Research','NewData','GCSP','Non-Tech'];
+      const List = LISTS.map(name => <li>{name}</li>);
+      return (
+        <div className=''>
+      
+          <div className='running-text'>
+            <p>Sign up for Virtual Chats here. | View Academic Calendar 2022-23 here. | Admissions for TLP Class of 2024 are now open. Click here to Apply</p>
+          </div>
+          <Form />
+          <div className='upperNav'>
+            <div className='left'>
+              <div className='logo'>
+                <img src={logo} />
+              </div>
+            </div>
+            <div className='mid'>
+              <nav>
+                <ul>
+               {List}
+                </ul>
+              </nav>
+            </div>
+            <div className='right'>
+              <button>Apply now</button>
+            </div>
+          </div>
+          <div className='lowernav'>
+            <ul>
+              <li>
+                Home
+              </li>
+            </ul>
+          </div>
+          {(this.state.video) ? <video src={this.props.vdo} autoplay className='vedio' /> : <img src={this.props.img}  className='image'/>}
+        </div>
+      )
+    }
+  }
+
+export default Navbar;
---
+++ src/Pages/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+        <h1>Form Contr</h1>
+        <form>
+            <input type='text' />
+            
+        </form>
+    </div>
+  )
+}
+
+export default Form
